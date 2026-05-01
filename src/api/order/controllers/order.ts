f76165ab00aa077994f0b4b6a90bcc6814ed065f
Matheus <matheus150101miranda@gmail.com>
2026-05-01 09:58:38 -0300

    feat: search Cart Game info

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -4,8 +4,27 @@
 
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController("api::order.order", () => ({
-  async createPaymentIntent(ctx) {
-    return "Hello World!";
-  },
-}));
+export default factories.createCoreController(
+  "api::order.order",
+  ({ strapi }) => ({
+    async createPaymentIntent(ctx) {
+      const { cart } = ctx.request.body;
+
+      let games = [];
+
+      await Promise.all(
+        cart?.map(async (game) => {
+          const validatedGame = await strapi
+            .query("api::game.game")
+            .findOne({ where: { id: game.id } });
+
+          if (validatedGame) {
+            games.push(validatedGame);
+          }
+        }),
+      );
+
+      return games;
+    },
+  }),
+);
