a77f802e4a0f98bc14cedcfba5016cf3b9f6aa46
Matheus <matheus150101miranda@gmail.com>
2026-05-24 11:46:29 -0300

    fix: adjustments test

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -1,16 +1,18 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
+      provider: env("CLOUDINARY_NAME") ? "cloudinary" : "local",
+      ...(env("CLOUDINARY_NAME") && {
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      }),
     },
   },
 });
